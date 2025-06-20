0feee26669f081e1341fbacb5cebaefeb4d22833
oseji10 <57804483+oseji10@users.noreply.github.com>
2025-06-20 02:02:41 +0100

    updates

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,43 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://portal.sokotopalliativeshop.com.ng',
-        'http://localhost:3000',
-    ],
+
+    'allowed_origins' => ['*', 'http://localhost:3000'],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
-    'max_age' => 1728000,
+
+    'max_age' => 0,
+
     'supports_credentials' => true,
+
+
+//     'paths' => ['api/*'],
+// 'allowed_methods' => ['*'],
+// 'allowed_origins' => ['http://your-nextjs-app.com'],
+// 'allowed_headers' => ['*'],
+// 'exposed_headers' => [],
+// 'max_age' => 0,
+// 'supports_credentials' => true, // Allow cookies
+
 ];
