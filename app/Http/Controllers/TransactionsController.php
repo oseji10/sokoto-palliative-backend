84d0866d35def5f027199a924c9c6f51d9f6383a
oseji10 <57804483+oseji10@users.noreply.github.com>
2025-06-23 13:40:13 +0100

    updates

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -87,7 +87,9 @@ class TransactionsController extends Controller
             if ($request->paymentMethod === 'outright') {
     $totalCostInKobo = $totalCost;
 
-    $moniepointResponse = Http::withHeaders([
+    $moniepointResponse = Http::withOptions([
+    'verify' => false,
+])->withHeaders([
         'Authorization' => 'Bearer mptp_a72e62d6220b4c279f05f0d90c71f79b_cce5ff',
         'Cookie' => '__cf_bm=llJAllZZ4ww_EAgd7WsHAiW9Xhdt5tOKkWsvByK6X2c-1750629087-1.0.1.1-2zOUQHrb5PyiYLrXqoA6kiONrHhKIZ2z7ifHO.iSk1Ue539LjL8bhuUWeZ7RaafQfCvMnh9Ke08Ks7Kkt4k0T2H0uJb89.aTwZt52.qkpyM'
     ])->post('https://api.pos.moniepoint.com/v1/transactions', [
