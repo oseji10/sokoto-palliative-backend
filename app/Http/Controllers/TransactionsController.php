e097fb82550e03ad891af1be9344a02b34cb0c79
oseji10 <57804483+oseji10@users.noreply.github.com>
2025-06-23 12:29:31 +0100

    updates

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -89,7 +89,7 @@ class TransactionsController extends Controller
 
     $moniepointResponse = Http::withHeaders([
         'Authorization' => 'Bearer mptp_a72e62d6220b4c279f05f0d90c71f79b_cce5ff',
-        'Cookie' => '__cf_bm=your_cookie_here'
+        'Cookie' => '__cf_bm=llJAllZZ4ww_EAgd7WsHAiW9Xhdt5tOKkWsvByK6X2c-1750629087-1.0.1.1-2zOUQHrb5PyiYLrXqoA6kiONrHhKIZ2z7ifHO.iSk1Ue539LjL8bhuUWeZ7RaafQfCvMnh9Ke08Ks7Kkt4k0T2H0uJb89.aTwZt52.qkpyM'
     ])->post('https://api.pos.moniepoint.com/v1/transactions', [
         'terminalSerial' => 'P260302358597',
         'amount' => $totalCostInKobo,
@@ -99,17 +99,17 @@ class TransactionsController extends Controller
     ]);
 
     // ✅ Check if the request was successful
-    if ($moniepointResponse->code() === 202) {
+    if ($moniepointResponse->status() === 202) {
         return response()->json([
             'status' => 'success',
             'message' => 'Payment request accepted by Moniepoint.',
-            'moniepoint_status' => $moniepointResponse->code(),
+            'moniepoint_status' => $moniepointResponse->status(),
             'moniepoint_description' => 'Accepted'
         ], 202);
     } else {
         // Log full Moniepoint response for debugging
         \Log::error('Moniepoint failed', [
-            'status' => $moniepointResponse->code(),
+            'status' => $moniepointResponse->status(),
             'body' => $moniepointResponse->body()
         ]);
 
