c7f3055197f1dab96691ac42c4ba2ffbc6aa6222
oseji10 <57804483+oseji10@users.noreply.github.com>
2025-06-25 03:10:23 +0100

    message

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,43 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'login', 'logout', '*'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://api.makenstream.com',
-        'http://localhost:3000' // optional for local testing
-    ],
-
-    'allowed_headers' => ['*'],
-
-    'supports_credentials' => true,
+    'allowed_origins' => ['*', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
+
+    'supports_credentials' => true,
+
+
+//     'paths' => ['api/*'],
+// 'allowed_methods' => ['*'],
+// 'allowed_origins' => ['http://your-nextjs-app.com'],
+// 'allowed_headers' => ['*'],
+// 'exposed_headers' => [],
+// 'max_age' => 0,
+// 'supports_credentials' => true, // Allow cookies
+
 ];
